132c39c5f40d72023412cf6a34d2de165db29cb2
TobiLean <tslean@outlook.com>
2023-10-10 12:32:53 +0100

    changed the twon to two

--- lab2ex2.cpp
+++ lab2ex2.cpp
@@ -3,7 +3,7 @@
 
 int main(){
     int x, y;
-    std::cout << "Please enter twon numbers: ";
+    std::cout << "Please enter two numbers: ";
     std::cin >> x >> y;
     std::cout << "the sum of " << x << "and" << y << "is: " << x + y << std::endl;
     std::cout << "the difference of " << x << "and" << y << "is: " << x - y << std::endl;
